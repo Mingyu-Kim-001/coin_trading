ae0c941c1c561ae4f9cc4a2fc902cea676156d03
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-05-25 22:38:49 +0900

    add momentum n day strategy

--- alpha_collection.py
+++ alpha_collection.py
@@ -1,7 +1,7 @@
 import pandas as pd
 class Alphas:
     def __init__(self):
-        self.alpha_list = ['simple_momentum_strategy', 'simple_regression_strategy']
+        self.alpha_list = ['simple_momentum_strategy', 'simple_regression_strategy', 'simple_momentum_nday_strategy']
 
     def simple_momentum_strategy(self, dict_df_klines: dict):
         df_agg = pd.concat(
@@ -13,6 +13,13 @@ class Alphas:
     def simple_regression_strategy(self, dict_df_klines: dict):
         df_agg = pd.concat(
             [-df_klines['close'].astype('float').pct_change().shift(1).rename(f'{symbol}_weight') for symbol, df_klines
+             in dict_df_klines.items()], axis=1)
+        df_rank = df_agg.rank(axis=1)
+        return df_rank
+
+    def simple_momentum_nday_strategy(self, dict_df_klines: dict, n=3):
+        df_agg = pd.concat(
+            [df_klines['close'].astype('float').pct_change().shift(n).rename(f'{symbol}_weight') for symbol, df_klines
              in dict_df_klines.items()], axis=1)
         df_rank = df_agg.rank(axis=1)
         return df_rank
