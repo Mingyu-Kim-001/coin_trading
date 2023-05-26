c352d922ed1e6d908d9468412b2591df678c81c9
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-05-26 22:20:42 +0900

    integrate redundant functions and add simple regression nday

--- alpha_collection.py
+++ alpha_collection.py
@@ -3,20 +3,6 @@ class Alphas:
     def __init__(self):
         pass
 
-    def simple_momentum_strategy(self, dict_df_klines: dict):
-        df_agg = pd.concat(
-            [df_klines['close'].astype('float').pct_change().shift(1).rename(f'{symbol}_weight') for symbol, df_klines
-             in dict_df_klines.items()], axis=1)
-        df_rank = df_agg.rank(axis=1)
-        return df_rank
-
-    def simple_regression_strategy(self, dict_df_klines: dict):
-        df_agg = pd.concat(
-            [-df_klines['close'].astype('float').pct_change().shift(1).rename(f'{symbol}_weight') for symbol, df_klines
-             in dict_df_klines.items()], axis=1)
-        df_rank = df_agg.rank(axis=1)
-        return df_rank
-
     def simple_momentum_nday_strategy(self, dict_df_klines: dict, n=3):
         df_agg = pd.concat(
             [df_klines['close'].astype('float').pct_change().shift(n).rename(f'{symbol}_weight') for symbol, df_klines
@@ -24,3 +10,9 @@ class Alphas:
         df_rank = df_agg.rank(axis=1)
         return df_rank
 
+    def simple_regression_nday_strategy(self, dict_df_klines: dict, n=3):
+        df_agg = pd.concat(
+            [-df_klines['close'].astype('float').pct_change().shift(n).rename(f'{symbol}_weight') for symbol, df_klines
+             in dict_df_klines.items()], axis=1)
+        df_rank = df_agg.rank(axis=1)
+        return df_rank
