b30f903c4e64a2243c75e2e735bda5b5d21390ff
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-05-29 13:56:55 +0900

    default 1 day for momentum and regression

--- alpha_collection.py
+++ alpha_collection.py
@@ -3,14 +3,14 @@ class Alphas:
     def __init__(self):
         pass
 
-    def simple_momentum_nday_strategy(self, dict_df_klines: dict, n=3):
+    def simple_momentum_nday_strategy(self, dict_df_klines: dict, n=1):
         df_agg = pd.concat(
             [df_klines['close'].astype('float').pct_change().shift(n).rename(f'{symbol}_weight') for symbol, df_klines
              in dict_df_klines.items()], axis=1)
         df_rank = df_agg.rank(axis=1)
         return df_rank
 
-    def simple_regression_nday_strategy(self, dict_df_klines: dict, n=3):
+    def simple_regression_nday_strategy(self, dict_df_klines: dict, n=1):
         df_agg = pd.concat(
             [-df_klines['close'].astype('float').pct_change().shift(n).rename(f'{symbol}_weight') for symbol, df_klines
              in dict_df_klines.items()], axis=1)
