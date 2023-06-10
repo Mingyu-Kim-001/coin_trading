3421658e819ba0c7111dc4305b9e87e1c814cce1
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-06-10 20:21:18 +0900

    add futures trading rules generation

---
+++ data_generation/get_futures_exchange_info.py
@@ -0,0 +1,37 @@
+import requests
+import pandas as pd
+
+def get_futures_trading_rules():
+    url = 'https://fapi.binance.com/fapi/v1/exchangeInfo'  # API endpoint for futures trading rules
+    response = requests.get(url)
+    data = response.json()
+
+    symbols_data = []
+    for symbol_data in data['symbols']:
+        symbol = symbol_data['symbol']
+        min_qty = None
+        min_notional = None
+        tick_size = None
+        for rule in symbol_data['filters']:
+            if rule['filterType'] == 'LOT_SIZE':
+                min_qty = float(rule['minQty'])
+            elif rule['filterType'] == 'MIN_NOTIONAL':
+                min_notional = float(rule['notional'])
+            elif rule['filterType'] == 'PRICE_FILTER':
+                tick_size = float(rule['tickSize'])
+
+        symbols_data.append({
+            'symbol': symbol,
+            'min_qty': min_qty,
+            'min_notinoal': min_notional,
+            'tick_size': tick_size
+        })
+
+    return symbols_data
+
+# Retrieve futures trading rules
+trading_rules = get_futures_trading_rules()
+
+# Create DataFrame
+df = pd.DataFrame(trading_rules)
+df.to_csv('../futures_trading_rules/futures_trading_rules.csv', index=False)
