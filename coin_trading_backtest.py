63315ed51e8fbc1f9e568b999e18f71cdf25483e
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-06-20 20:10:24 +0900

    add leverage to backtest

--- coin_trading_backtest.py
+++ coin_trading_backtest.py
@@ -9,7 +9,7 @@ class market_neutral_trading_backtest_binance:
     def __init__(self):
         self.initial_budget = 10000
         self.trade_ratio = 0.1
-        self.fee_rate = 0.00016  # 0.016%
+        self.fee_rate = 0.0002  # 0.02%
 
     def get_binance_klines_data_1d(self, symbol, start_date='2017-01-01', end_date='2022-05-01', is_future=False):
         with open(f'./coin_backdata_daily/{"f" + symbol if is_future else symbol}.csv', 'r') as f:
@@ -19,9 +19,11 @@ class market_neutral_trading_backtest_binance:
         df_extended = df_date_dummy.merge(df, on='date', how='left').fillna(0)
         for column in ['open', 'high', 'low', 'close', 'volume']:
             df_extended[column] = df_extended[column].astype(float)
+        if symbol == 'DOGEUSDT':
+            df_extended = df_extended[df_extended['date'] >= '2022-01-01'] # remove volatile data
         return df_extended
 
-    def backtest_coin_strategy(self, df_neutralized_weight, dict_df_klines, df_date, symbols, stop_loss=-1):
+    def backtest_coin_strategy(self, df_neutralized_weight, dict_df_klines, df_date, symbols, stop_loss=-1, leverage=1):
         dict_df_return, dict_df_trade_size = {}, {}
         df_agg = pd.DataFrame(df_date, columns=['date'])
         for symbol in symbols:
@@ -48,8 +50,8 @@ class market_neutral_trading_backtest_binance:
                                   abs(df_neutralized_weight_symbol - df_neutralized_weight_symbol_lag)))
             )
 
-        df_agg['return'] = pd.DataFrame(dict_df_return).sum(axis=1)
-        df_agg['trade_size'] = pd.DataFrame(dict_df_trade_size).sum(axis=1)
+        df_agg['return'] = pd.DataFrame(dict_df_return).sum(axis=1) * leverage
+        df_agg['trade_size'] = pd.DataFrame(dict_df_trade_size).sum(axis=1) * leverage
         df_agg['fee'] = df_agg['trade_size'].mul(self.fee_rate)
         df_agg['return_net'] = (1 - df_agg['fee']) * (1 + df_agg['return']) - 1
         df_agg['cumulative_fee'] = df_agg['fee'].cumsum()
--- run_backtest.py
+++ run_backtest.py
@@ -8,13 +8,15 @@ pd.set_option('display.width', 1000)
 backtest = market_neutral_trading_backtest_binance()
 alpahs = Alphas()
 alpha_org_names = [alpha_name for alpha_name in alpahs.__dir__() if not alpha_name.startswith('_')]
+alpha_org_names = ['close_momentum_nday']
 dict_alphas = {}
 for alpha_name in alpha_org_names:
     if 'nday' in alpha_name:
-        for n in list(range(1, 5)) + [10, 20, 50, 100, 200]:
+        for n in list(range(1, 5)):# + [10, 20, 50, 100, 200]:
             dict_alphas[alpha_name + f'_{n}'] = (lambda name, n: lambda x: getattr(alpahs, name)(x, n))(alpha_name, n)
-            if alpha_name == 'close_momentum_nday':
-                dict_alphas[alpha_name + f'_{n}_max_weight_0.15'] = (lambda name, n: lambda x: getattr(alpahs, name)(x, n, max_weight=0.15))(alpha_name, n)
+            # if alpha_name == 'close_momentum_nday':
+            #     max_weight = 0.5
+            #     dict_alphas[alpha_name + f'_{n}_max_weight_{max_weight}'] = (lambda name, n: lambda x: getattr(alpahs, name)(x, n, max_weight=max_weight))(alpha_name, n)
     else:
         dict_alphas[alpha_name] = getattr(alpahs, alpha_name)
 
@@ -38,22 +40,26 @@ for alpha_name, alpha in dict_alphas.items():
     df_weight_recent.loc[df_date <= pd.to_datetime(past_recent_split_date), :] = 0
     final_return, possible_maximum_drawdown = [] , []
     for df_weight, is_past in zip([df_weight_past, df_weight_recent], [True, False]):
-        backtest_result = backtest.backtest_coin_strategy(df_weight, dict_df_klines, df_date, symbols)
+        backtest_result = backtest.backtest_coin_strategy(df_weight, dict_df_klines, df_date, symbols, leverage=4)
         final_return.append(round(backtest_result['cumulative_return'].iloc[-1], 2))
         possible_maximum_drawdown.append(round(backtest_result['possible_maximum_drawdown'].min(), 2))
     print(alpha_name, 'final return', final_return, 'possible_maximum_drawdown', possible_maximum_drawdown, [f'{start_date} ~ {past_recent_split_date}', f'{past_recent_split_date} ~ {end_date}'])
 
 
 print('-------------------')
-print(f'future {past_recent_split_date} ~ {end_date}')
+start_date = datetime.date(2017, 8, 17)
+future_start_date = datetime.date(2022, 1, 1)
+future_end_date = datetime.date(2022, 6, 1)
+print(f'future {future_start_date} ~ {future_end_date}')
 for symbol in symbols:
-    dict_df_klines[symbol] = backtest.get_binance_klines_data_1d(symbol, start_date, end_date, is_future=True)
+    dict_df_klines[symbol] = backtest.get_binance_klines_data_1d(symbol, start_date, future_end_date, is_future=True)
+df_date = list(dict_df_klines.values())[0]['date']
 for alpha_name, alpha in dict_alphas.items():
     df_weight = alpha(dict_df_klines)
     df_weight_recent = df_weight.copy()
-    df_weight_recent.loc[df_date <= pd.to_datetime(past_recent_split_date), :] = 0
-    backtest_result = backtest.backtest_coin_strategy(df_weight_recent, dict_df_klines, df_date, symbols)
+    df_weight_recent.loc[(df_date < pd.to_datetime(future_start_date)) | (df_date > pd.to_datetime(future_end_date)), :] = 0
+    backtest_result = backtest.backtest_coin_strategy(df_weight_recent, dict_df_klines, df_date, symbols, leverage=2)
     final_return = round(backtest_result['cumulative_return'].iloc[-1], 2)
     possible_maximum_drawdown = round(backtest_result['possible_maximum_drawdown'].min(), 2)
-    print(alpha_name, 'final return', final_return, 'possible_maximum_drawdown', possible_maximum_drawdown, f'{past_recent_split_date} ~ {end_date}')
+    print(alpha_name, 'final return', final_return, 'possible_maximum_drawdown', possible_maximum_drawdown, f'{future_start_date} ~ {future_end_date}')
 
