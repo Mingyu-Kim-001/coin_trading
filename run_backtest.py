9206a4f4ebb99bf84c0bb73e68ba28738712672d
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-06-27 19:04:28 +0900

    add DOTUSDT

--- run_backtest.py
+++ run_backtest.py
@@ -16,15 +16,15 @@ pd.set_option('display.width', 1000)
 backtest = market_neutral_trading_backtest_binance()
 alpahs = Alphas()
 # alpha_org_names = [alpha_name for alpha_name in alpahs.__dir__() if not alpha_name.startswith('_')]
-alpha_org_names = ['close_momentum_nday', 'bollinger_band_nday']
+alpha_org_names = ['bollinger_band_nday']
 dict_alphas = {}
 for alpha_name in alpha_org_names:
     if 'nday' in alpha_name:
-        for n in [2, 3, 4, 5, 10, 20]:
+        for n in [4, 20]:
         # for n in list(range(1, 5)):# + [10, 20, 50, 100, 200]:
             dict_alphas[alpha_name + f'_{n}'] = (lambda name, n: lambda x: getattr(alpahs, name)(x, n))(alpha_name, n)
             # if alpha_name == 'close_momentum_nday':
-            # for weight_max in [0.01, 0.02, 0.03, 0.04, 0.05, 0.06, 0.07, 0.08]:
+            # for weight_max in [0.5, 0.7, 0.9, 1, 1.5]:
             #     dict_alphas[alpha_name + f'_{n}_weight_max_{weight_max}'] = (lambda name, n, weight_max: lambda x: getattr(alpahs, name)(x, n, weight_max))(alpha_name, n, weight_max)
     else:
         dict_alphas[alpha_name] = getattr(alpahs, alpha_name)
@@ -35,8 +35,8 @@ start_date = datetime.date(2017, 8, 17)
 past_recent_split_date = datetime.date(2023, 1, 1)
 end_date = datetime.date(2023, 6, 10)
 print(f'spot {start_date} ~ {end_date}')
-symbols = ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'DOGEUSDT', 'LTCUSDT', 'MATICUSDT', 'TRXUSDT', 'ADAUSDT', 'SOLUSDT']#, 'BNBUSDT', 'DOTUSDT']
-leverage = 4
+symbols = ['BTCUSDT', 'ETHUSDT', 'XRPUSDT', 'DOGEUSDT', 'LTCUSDT', 'MATICUSDT', 'TRXUSDT', 'ADAUSDT', 'SOLUSDT', 'DOTUSDT']#, 'BNBUSDT', 'DOTUSDT']
+leverage = 5 * 0.9
 # symbols = ['BTCUSDT', 'ETHUSDT']
 
 for symbol in symbols:
@@ -60,16 +60,17 @@ for alpha_name, alpha in dict_alphas.items():
 print('-------------------')
 dict_df_klines_futures = {}
 future_start_date = datetime.date(2019, 9, 8)
-future_past_recent_split_date = datetime.date(2023, 1, 1)
+future_past_recent_split_date = datetime.date(2022, 1, 1)
 future_end_date = datetime.date(2023, 6, 10)
 for symbol in symbols:
     dict_df_klines_futures[symbol] = backtest.get_binance_klines_data_1d(symbol, future_start_date, future_end_date, is_future=True)
 print(f'future {future_start_date} ~ {future_end_date}')
 df_date = list(dict_df_klines_futures.values())[0]['date']
+df_weights = {}
 for alpha_name, alpha in dict_alphas.items():
-    df_weight = alpha(dict_df_klines_futures)
-    df_weight_past = df_weight.copy()
-    df_weight_recent = df_weight.copy()
+    df_weights[alpha_name] = alpha(dict_df_klines_futures)
+    df_weight_past = df_weights[alpha_name].copy()
+    df_weight_recent = df_weights[alpha_name].copy()
     df_weight_past.loc[df_date >= pd.to_datetime(future_past_recent_split_date), :] = 0
     df_weight_recent.loc[df_date < pd.to_datetime(future_past_recent_split_date), :] = 0
     final_return, possible_maximum_drawdown, win_day_rate = [] , [], []
@@ -82,7 +83,9 @@ for alpha_name, alpha in dict_alphas.items():
         start_date = future_start_date if is_past else future_past_recent_split_date
         end_date = future_past_recent_split_date if is_past else future_end_date
         save_backtest_result_figure(backtest_result, alpha_name, start_date, end_date, leverage=leverage)
-    print(alpha_name, 'final return', final_return, 'possible_maximum_drawdown', possible_maximum_drawdown, [f'{future_start_date} ~ {future_past_recent_split_date}', f'{future_past_recent_split_date} ~ {future_end_date}'], 'win_day_rate', win_day_rate)
+    print(alpha_name,'whole day final return', round(final_return[0] * final_return[1], 2), 'final return', final_return, 'possible_maximum_drawdown', possible_maximum_drawdown, [f'{future_start_date} ~ {future_past_recent_split_date}', f'{future_past_recent_split_date} ~ {future_end_date}'], 'win_day_rate', win_day_rate)
+
+print('-------------------')
 
 
 
