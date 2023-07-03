1042aaaee3cef79edf30a78bd9b9d3f8a07a82ff
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-07-03 22:55:27 +0900

    change shell command

--- run_trading.sh
+++ run_trading.sh
@@ -4,4 +4,4 @@ budget_allocation=$3
 source ~/.zshrc
 cd ~/PycharmProjects/coin_trading
 source ./venv/bin/activate
-python3 ./run_trading.py --dryrun $dryrun --leverage $leverage  --budget_allocation $budget_allocation --api_key $BINANCE_API_KEY --api_secret $BINANCE_SECRET_API_KEY
+python3 ./run_trading.py --dryrun "$dryrun" --leverage "$leverage"  --budget_allocation "$budget_allocation" --api_key "$BINANCE_API_KEY" --api_secret "$BINANCE_SECRET_API_KEY"
