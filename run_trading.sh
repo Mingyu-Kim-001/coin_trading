b1cd8ebcad817fb3da71dc0c83251dd6ad835749
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-07-09 15:22:39 +0900

    Revert "add home dir"
    This reverts commit b24b2477ca5eebce9e74cd7eb1bd841aa1d809d6.

--- run_trading.sh
+++ run_trading.sh
@@ -1,7 +1,7 @@
 dryrun=$1
 leverage=$2
 budget_allocation=$3
-cd $HOME/PycharmProjects/coin_trading
+cd ~/PycharmProjects/coin_trading
 source .env
 source ./venv/bin/activate
 python3 ./run_trading.py --dryrun "$dryrun" --leverage "$leverage"  --budget_allocation "$budget_allocation" --api_key "$BINANCE_API_KEY" --api_secret "$BINANCE_SECRET_API_KEY" --slack_token "$SLACK_TOKEN"
