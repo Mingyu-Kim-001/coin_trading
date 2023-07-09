0ee0bc005033fa19d9c2840a5d602a2abe35d529
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-07-10 00:16:13 +0900

    remove pwd

--- run_trading.sh
+++ run_trading.sh
@@ -2,7 +2,6 @@ dryrun=$1
 leverage=$2
 budget_allocation=$3
 cd ~/PycharmProjects/coin_trading
-pwd
 source ./env_file
 source ./venv/bin/activate
 python3 ./run_trading.py --dryrun "$dryrun" --leverage "$leverage"  --budget_allocation "$budget_allocation" --api_key "$BINANCE_API_KEY" --api_secret "$BINANCE_SECRET_API_KEY" --slack_token "$SLACK_TOKEN"
