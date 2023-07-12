1ad63668c38f0be3f6eb6eac1ad3b0228ff794d1
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-07-12 22:23:12 +0900

    Revert "add git pull"
    This reverts commit b42fe88c1bc30571fa875f6861293c997373a828.

--- run_trading.sh
+++ run_trading.sh
@@ -4,7 +4,6 @@ budget_allocation=$3
 budget_keep=$4
 script_dir=$(dirname "$0")
 cd "$script_dir" || exit
-git pull origin master
 source ./env_file
 source ./venv/bin/activate
 python3 ./run_trading.py --dryrun "$dryrun" --leverage "$leverage"  --budget_allocation "$budget_allocation" --budget_keep "$budget_keep" --api_key "$BINANCE_API_KEY" --api_secret "$BINANCE_SECRET_API_KEY" --slack_token "$SLACK_TOKEN"
