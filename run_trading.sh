5dcbe40c13507dc8a881b779bb4668acf4219b56
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-07-13 22:24:46 +0900

    add date to log

--- run_trading.sh
+++ run_trading.sh
@@ -6,4 +6,5 @@ script_dir=$(dirname "$0")
 cd "$script_dir" || exit
 source ./env_file
 source ./venv/bin/activate
+echo `date`
 python3 ./run_trading.py --dryrun "$dryrun" --leverage "$leverage"  --budget_allocation "$budget_allocation" --budget_keep "$budget_keep" --api_key "$BINANCE_API_KEY" --api_secret "$BINANCE_SECRET_API_KEY" --slack_token "$SLACK_TOKEN"
