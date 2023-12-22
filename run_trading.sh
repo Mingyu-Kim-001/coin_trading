a393ee5f215ab83521bcaa1a906442e0cbae944e
Mingyu-Kim-001 <aksen0@snu.ac.kr>
2023-12-22 20:07:22 +0900

    prod

--- run_trading.sh
+++ run_trading.sh
@@ -8,4 +8,5 @@ cd "$script_dir" || exit
 . ./env
 eval "${PYTHON_VIRTUALENV_ACTIVATE_COMMAND}"
 echo `date`
+git pull origin master
 python3 ./run_trading.py --dryrun "$dryrun" --leverage "$leverage"  --budget_allocation "$budget_allocation" --budget_keep "$budget_keep" --api_key "$BINANCE_API_KEY" --api_secret "$BINANCE_SECRET_API_KEY" --slack_token "$SLACK_TOKEN"
